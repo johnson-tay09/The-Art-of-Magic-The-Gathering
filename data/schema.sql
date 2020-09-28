566d81000f4eee0855a4b7ee320d92c940807b3d
TAYLOR JOHNSON <TAYLOR.MCMINN@GMAIL.COM>
2020-09-28 13:42:50 -0700

    routes one

---
+++ data/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS magic_table;
+
+CREATE TABLE magic_table (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  artist VARCHAR(255),
+  image_url VARCHAR(255),
+  artist_store_url VARCHAR(255)
+);
